97a9c75c72a0dac4e85bc0eb2c1ee282bc1b07a8
Stephen Ng <stephen5.ng@gmail.com>
2024-02-08 20:21:36 -0800

    Add numpy to setup.py

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
     ],
     install_requires=[
         'bdfparser<=2.2.0',
+        'numpy>=1.2.0',
         'pygame>=2.0.1,<3',
         'tornado>=6.1',
         'libsixel-python>=0.5.0',
