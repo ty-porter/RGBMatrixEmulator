7a8327fecf4b3bc998d076227ed72fdf04a2670e
Tyler Porter <tyler.b.porter@gmail.com>
2025-11-19 14:12:07 -0500

    Add no-cache headers to browser adapter

---
+++ RGBMatrixEmulator/adapters/browser_adapter/request_handlers/base.py
@@ -0,0 +1,21 @@
+import tornado.web
+
+
+class NoCacheRequestHandler(tornado.web.RequestHandler):
+    """Base handler that adds no-cache headers to all responses."""
+
+    def set_default_headers(self):
+        """Set headers to prevent browser caching."""
+        self.set_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.set_header("Pragma", "no-cache")
+        self.set_header("Expires", "0")
+
+
+class NoCacheStaticFileHandler(tornado.web.StaticFileHandler):
+    """Static file handler that adds no-cache headers to all responses."""
+
+    def set_default_headers(self):
+        """Set headers to prevent browser caching."""
+        self.set_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.set_header("Pragma", "no-cache")
+        self.set_header("Expires", "0")
