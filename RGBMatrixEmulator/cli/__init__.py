a85809e3610a07012534a8a9e5d3f1482073333a
Tyler Porter <tyler.b.porter@gmail.com>
2026-03-08 23:51:30 -0400

    Add a CLI interface to create new configs

---
+++ RGBMatrixEmulator/cli/__init__.py
@@ -0,0 +1,34 @@
+import argparse, sys
+
+from RGBMatrixEmulator.cli.config import ConfigCLICommand
+from RGBMatrixEmulator.logger import Logger
+
+class CLI:
+    COMMANDS = {
+        # Full commands
+        "config": ConfigCLICommand,
+        # Aliases
+        "c": ConfigCLICommand,
+    }
+
+    @staticmethod
+    def execute():
+        parser = argparse.ArgumentParser(
+            description="RGBMatrixEmulator command line interface."
+        )
+        subparsers = parser.add_subparsers(dest="command", required=True, help="Available commands")
+
+        # "config" command
+        subparsers.add_parser("config", aliases=["c"], help="Initialize and manipulate emulator_config.json files")
+
+        args = parser.parse_args()
+
+        if args.command not in CLI.COMMANDS:
+            Logger.critical(f"Error: Unknown command '{args.command}'")
+            sys.exit(1)
+
+        cmd = CLI.COMMANDS[args.command]
+        cmd(args).execute()
+
+def run_cli():
+    CLI.execute()
---
+++ RGBMatrixEmulator/cli/cli.py
@@ -0,0 +1,2 @@
+def run_cli():
+    pass
