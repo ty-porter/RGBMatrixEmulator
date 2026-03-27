b9d4833d4399ed92495297164581bfdfa45d3da1
Brian Ward <brianmward99@gmail.com>
2026-03-26 23:01:19 -0400

    Add public-facing types to graphics and canvas APIs (#127)

--- RGBMatrixEmulator/emulation/matrix.py
+++ RGBMatrixEmulator/emulation/matrix.py
@@ -1,59 +1,66 @@
+from PIL import Image
+
 from RGBMatrixEmulator.emulation.canvas import Canvas
+from RGBMatrixEmulator.emulation.options import RGBMatrixOptions
 
 
 class RGBMatrix:
-    def __init__(self, options={}):
+    def __init__(self, options: RGBMatrixOptions = RGBMatrixOptions()) -> None:
         self.options = options
 
         self.width = options.cols * options.chain_length
         self.height = options.rows * options.parallel
 
-        self.canvas = None
-
-    def CreateFrameCanvas(self):
+    def CreateFrameCanvas(self) -> Canvas:
         self.canvas = Canvas(options=self.options)
 
         return self.canvas
 
     # TODO: framerate_fraction support not yet implemented
     # https://github.com/hzeller/rpi-rgb-led-matrix/commit/f88355e46faafb9de7f1e59dd258ab36c7e7b097
-    def SwapOnVSync(self, canvas, *other):
+    def SwapOnVSync(self, canvas: Canvas, framerate_fraction: int = 1) -> Canvas:
         canvas.check_for_quit_event()
         canvas.draw_to_screen()
         self.canvas = canvas
 
         return self.canvas
 
-    def Clear(self):
+    def Clear(self) -> None:
         self.__sync_canvas()
         self.canvas.Clear()
         self.SwapOnVSync(self.canvas)
 
-    def Fill(self, r, g, b):
+    def Fill(self, r: int, g: int, b: int) -> None:
         self.__sync_canvas()
         self.canvas.Fill(r, g, b)
         self.SwapOnVSync(self.canvas)
 
-    def SetPixel(self, x, y, r, g, b):
+    def SetPixel(self, x: int, y: int, r: int, g: int, b: int) -> None:
         self.__sync_canvas()
         self.canvas.SetPixel(x, y, r, g, b)
         self.SwapOnVSync(self.canvas)
 
-    def SetImage(self, image, offset_x=0, offset_y=0, *other):
+    def SetImage(
+        self,
+        image: Image.Image,
+        offset_x: int = 0,
+        offset_y: int = 0,
+        unsafe: bool = True,
+    ) -> None:
         self.__sync_canvas()
-        self.canvas.SetImage(image, offset_x, offset_y, *other)
+        self.canvas.SetImage(image, offset_x, offset_y, unsafe)
         self.SwapOnVSync(self.canvas)
 
-    def __sync_canvas(self):
+    def __sync_canvas(self) -> None:
         if not self.canvas:
             self.canvas = Canvas(options=self.options)
 
     @property
-    def brightness(self):
+    def brightness(self) -> int:
         return self.options.brightness
 
     @brightness.setter
-    def brightness(self, value):
+    def brightness(self, value: int) -> None:
         if not isinstance(value, (int, float)):
             raise ValueError(f"brightness must be a numeric value, received '{value}'")
         elif value < 0 or value > 100:
--- RGBMatrixEmulator/graphics/color.py
+++ RGBMatrixEmulator/graphics/color.py
@@ -1,5 +1,5 @@
 class Color:
-    def __init__(self, r=0, g=0, b=0):
+    def __init__(self, r: int = 0, g: int = 0, b: int = 0) -> None:
         # Simulate Cython uint8_t bounds checking
         for c in (r, g, b):
             if c > 255:
@@ -11,30 +11,3 @@ class Color:
         self.red = r
         self.green = g
         self.blue = b
-
-    @classmethod
-    def adjust_brightness(cls, pixel, alpha, to_int=False):
-        if to_int:
-            return tuple(int(channel) for channel in pixel)
-
-        return tuple(channel * alpha for channel in pixel)
-
-    @classmethod
-    def to_hex(cls, pixel):
-        return "#%02x%02x%02x" % pixel
-
-    @classmethod
-    def BLACK(cls):
-        return (0, 0, 0)
-
-    @classmethod
-    def RED(cls):
-        return (255, 0, 0)
-
-    @classmethod
-    def GREEN(cls):
-        return (0, 255, 0)
-
-    @classmethod
-    def BLUE(cls):
-        return (0, 0, 255)
